10ac072541a5155037e774a1bc8a84dd010711d6
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-10-29 16:41:37 -0300

    Feat: cria compnente form

---
+++ src/components/Form.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './form.module.css';
+
+function Form() {
+  return (
+    <div className={style.container__form}>
+      <form />
+    </div>
+  );
+}
+
+export default Form;
