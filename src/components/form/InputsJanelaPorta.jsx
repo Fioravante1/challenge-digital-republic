70d28fd09dbd110681f94629e5b03ca877d90505
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-10-29 18:20:49 -0300

    Feat: Config do controle dos inputs janela porta

--- src/components/form/InputsJanelaPorta.jsx
+++ src/components/form/InputsJanelaPorta.jsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Context from '../../context/Context';
 
 function InputsJanelaPorta() {
+  const { dataInputs, setDataInputs } = useContext(Context);
+
+  console.log(dataInputs);
+
+  function handleOnChange(event) {
+    const { name, value } = event.target;
+    setDataInputs((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }
+
   return (
     <>
       <fieldset>
         <legend>Quantidade de janela?</legend>
-        <input type="number" placeholder="Quantidade de janela" />
+        <input
+          name="janela"
+          type="number"
+          placeholder="Quantidade de janela"
+          onChange={handleOnChange}
+        />
       </fieldset>
 
       <fieldset>
         <legend>Quantidade de porta?</legend>
-        <input type="number" placeholder="Quantidade de porta" />
+        <input
+          name="porta"
+          type="number"
+          placeholder="Quantidade de porta"
+          onChange={handleOnChange}
+        />
       </fieldset>
     </>
   );
