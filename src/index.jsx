ce9a9467c653b0f7e0bbf20e4ccaba0be8e84edb
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-10-29 18:19:45 -0300

    Feat: Add provider no index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,9 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
