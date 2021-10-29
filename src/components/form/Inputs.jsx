86512fa683b2b6b9d5dea1b0b317caf230900abd
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-10-29 16:56:36 -0300

    Feat: Cria componente inputs

---
+++ src/components/form/Inputs.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function Inputs() {
+  return (
+    <>
+      <fieldset>
+        <legend>Parede 1</legend>
+        <input type="number" />
+        <input type="number" />
+      </fieldset>
+
+      <fieldset>
+        <legend>Parede 2</legend>
+        <input type="number" />
+        <input type="number" />
+      </fieldset>
+
+      <fieldset>
+        <legend>Parede 3</legend>
+        <input type="number" />
+        <input type="number" />
+      </fieldset>
+
+      <fieldset>
+        <legend>Parede 4</legend>
+        <input type="number" />
+        <input type="number" />
+      </fieldset>
+    </>
+  );
+}
+
+export default Inputs;
