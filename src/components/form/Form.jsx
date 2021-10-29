1325f80be51ba510662765d87cdf390146189e60
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-10-29 17:18:05 -0300

    fixoStyle: Cria divs para auxiliar posicionamento dos inputs

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import style from './form.module.css';
+import InputsJanelaPorta from './InputsJanelaPorta';
 import InputsParedes from './InputsParedes';
 
 function Form() {
   return (
     <div className={style.container__form}>
       <form>
-        <InputsParedes />
+        <div className={style.container__paredes}>
+          <InputsParedes />
+        </div>
+        <div className={style.container__porta__janela}>
+          <InputsJanelaPorta />
+        </div>
       </form>
     </div>
   );
