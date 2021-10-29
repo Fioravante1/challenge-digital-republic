c9d1a53eca4af8ad2e50e59280fed82fad7013ac
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-10-29 17:15:50 -0300

    Feat: Cria componente de inputs para portas e janelas

---
+++ src/components/form/InputsJanelaPorta.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function InputsJanelaPorta() {
+  return (
+    <>
+      <fieldset>
+        <legend>Quantidade de janela?</legend>
+        <input type="number" />
+      </fieldset>
+
+      <fieldset>
+        <legend>Quantidade de porta?</legend>
+        <input type="number" />
+      </fieldset>
+    </>
+  );
+}
+
+export default InputsJanelaPorta;
