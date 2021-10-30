7acae5d3160457e61d94b6350ac1de0705180e15
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-10-29 21:10:04 -0300

    Feat: Cria Modal

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Modal from 'react-modal';
+import Context from '../../context/Context';
+import styles from './modal.module.css';
 
-function Modal() {
+function ModalResponse() {
+  const { show, setShow } = useContext(Context);
   return (
-    <div />
+    <Modal isOpen={show} className={styles.container__modal}>
+      <h1>Aqui está a resposta!</h1>
+      <button type="button" onClick={() => setShow(false)}>Fechar</button>
+    </Modal>
   );
 }
 
-export default Modal;
+export default ModalResponse;
