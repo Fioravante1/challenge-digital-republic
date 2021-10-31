811497a808136938c66edb1e7b47e2d3be4e61b5
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-10-31 18:58:23 -0300

    Refactor: Modal err

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,13 +1,20 @@
 import React, { useContext } from 'react';
 import Modal from 'react-modal';
 import Context from '../../context/Context';
+import areaTotalParedes from '../../helpers/areaTotal';
+import messageUser from '../../helpers/messageUser';
 import styles from './modal.module.css';
 
 function ModalResponse() {
-  const { show, setShow } = useContext(Context);
+  const { show, setShow, dataInputs } = useContext(Context);
+
+  const messagePersonalityUser = messageUser(dataInputs);
+  const litrosNecessrio = areaTotalParedes(dataInputs);
+
   return (
-    <Modal isOpen={show} className={styles.container__modal}>
-      <h1>Aqui está a resposta!</h1>
+    <Modal ariaHideApp={false} isOpen={show} className={styles.container__modal}>
+      <h1>{`Litros necessario: ${litrosNecessrio}L`}</h1>
+      <h1>{messagePersonalityUser}</h1>
       <button type="button" onClick={() => setShow(false)}>Fechar</button>
     </Modal>
   );
